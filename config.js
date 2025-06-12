d5703c6a4ef47f24e7a3cb98acc880740fd6a56b
Ana Carolina Rangel de Medeiros <142359637+satiromediros@users.noreply.github.com>
2025-06-12 00:29:30 -0300

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://youtu.be/hda5v8tFM28?si=77jtr8FVhF3i6gc4", // Music streaming URL
+        musicUrl: "https://player.cloudinary.com/embed/?cloud_name=dicki48wv&public_id=txz13ijn3r5ituq4jv0h&profile=cld-default", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
